06b188f7ef28d1922a87bc5bb2cd5355a0c0ed17
Maxi254 <ocholamaxwell543@gmail.com>
2023-12-03 15:06:06 -0800

    oaky

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -1,20 +1,19 @@
 #ifndef LISTS_H
 #define LISTS_H
-
 #include <stdlib.h>
 
 /**
- * struct listint_s - singly linked list
- * @n: integer
- * @next: points to the next node
+ * struct listint_s - ssl
+ * @n: int
+ * @next: pt to the nn
  *
  * Description: singly linked list node structure
  * for Holberton project
  */
 typedef struct listint_s
 {
-	int n;
-	struct listint_s *next;
+	int a;
+	struct listint_s *n;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
@@ -22,4 +21,4 @@ listint_t *add_nodeint(listint_t **head, const int n);
 void free_listint(listint_t *head);
 int check_cycle(listint_t *list);
 
-#endif /* LISTS_H */
+#endif
